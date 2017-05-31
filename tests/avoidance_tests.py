a4d6518b9a9e41e387352affa4b64f80107e2fee
Connor Dittmar <connordittmar@Connors-MacBook-Pro.local>
2017-05-31 08:23:40 -0400

    added test skeleton

---
+++ tests/avoidance_tests.py
@@ -0,0 +1,12 @@
+# Unit Tests for avoidance
+from bin import gpsutils
+from avoidance import logic
+from bin import
+
+def setup():
+    print 'Setup.'
+
+def run():
+    pring 'Running...'
+
+def test_collision():

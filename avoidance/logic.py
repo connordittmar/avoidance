f4274af9b773432101b267b20f09fc5a63ffc152
Moore <m184410@academy.usna.edu>
2017-06-01 10:14:58 -0400

    added waypoint algo

--- avoidance/logic.py
+++ avoidance/logic.py
@@ -1,5 +1,5 @@
 from bin import gpsutils
-from math import sqrt
+from math import sqrt, sin, cos, atan2
 
 def collision_check(plane_lla,object):
     helper = gpsutils.GpsUtils()
@@ -17,3 +17,28 @@ def calc_dist(enu):
     up = enu[2]
     calc_dist = sqrt(east**(2) + north**(2) + up**(2))
     return calc_dist
+
+def find_wp_enu(enu,apparent_radius,heading,look_ahead_dist):
+    #finds next waypoint after risk of collision becomes apparent
+    x=look_ahead_dist*sin(heading) #heading must be in radians
+    y=look_ahead_dist*cos(heading)
+    enu_look_ahead= [x , y]
+
+    for i in range(1,6): #i goes from 1 to 5
+        heading_temp=heading-(.88*i)
+        x=look_ahead_dist*sin(heading_temp) #heading must be in radians
+        y=look_ahead_dist*cos(heading_temp) #heading must be in radians
+        enu_look_ahead= [x , y]
+        dr_dist=diff_distance(enu_look_ahead,enu) #finding the distance the projected point will be from the object
+        if dr_dist > apparent_radius: #if distance to object is greater than safty dist and object radius
+            break
+        else:
+            heading_temp=heading+(.88*i) #checking other direction
+            x=look_ahead_dist*sin(heading_temp)
+            y=look_ahead_dist*cos(heading_temp)
+            enu_look_ahead= [x , y]
+            dr_dist=diff_distance(enu_look_ahead,enu)
+        if dr_dist > apparent_radius: 
+            break
+    wp=enu_look_ahead
+    return wp
